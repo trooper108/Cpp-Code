0a4fc3319975ba8cd14d715263bd1ecb5064d426
Vishal Pal <vishalpal1215@gmail.coml>
2023-03-10 22:13:29 +0530

    Selection Sort

---
+++ SelectionSort.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+void SelectionSort(int arr[], int n)
+{
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[minIndex] > arr[j])
+            {
+                minIndex = j;
+            }
+        }
+        if (minIndex != i)
+        {
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+    return;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    SelectionSort(arr,n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i]<<" ";
+    }
+    cout << endl;
+}
