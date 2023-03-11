47f67c446dc6cf5bb9fdc5f8a5a997b8c2995891
Vishal Pal <vishalpal1215@gmail.coml>
2023-03-11 14:17:40 +0530

    Insertion Sort

---
+++ InsertionSort.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void InsertionSort(vector<int> &arr)
+{
+    for (int i = 1; i < arr.size(); i++)
+    {
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+    return;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> arr;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        arr.push_back(x);
+    }
+
+    InsertionSort(arr);
+    for (auto it : arr)
+    {
+        cout << it << " ";
+    }
+    cout << endl;
+}
